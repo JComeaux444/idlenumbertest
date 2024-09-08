d9255d428121668b8ef02fd4910bd057b2fc43ce
JComeaux444 <129115698+JComeaux444@users.noreply.github.com>
2024-09-07 23:51:23 -0700

    Updated gold Methods
    Added formatted gold label. Should be updated and made more efficient ASAP too, too many for loops for what it is imo.

--- goldMethods.gd
+++ goldMethods.gd
@@ -1,14 +1,15 @@
 extends Node
 
+@onready var gold_text : Label = $TestLabel
 
 # Thousands handles 0-999999 so players can spend ex: 234, gold on random items 
 var gold = {
 	
 	"Thousands" : 500,
 	"Millions" : 997,
-	"Billions" : 999,
-	"Trillions" : 999,
-	"Quadrillion" : 999,
+	"Billions" : 0,
+	"Trillions" : 0,
+	"Quadrillion" : 0,
 	"Quintillion" : 0,
 	"Sextillion" : 0,
 	"Septillion" : 0,
@@ -22,18 +23,58 @@ var gold = {
 	
 }
 
+# only here to track timer. count++ when 1 sec passes
 var curTime = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	_update_goldText()
 	#print(gold)
-	pass
+	#pass
 
 func _process(delta: float) -> void:
 	pass
 
+# updates TestLabel on the UI only
+# Should format where it is gold[highestUsedOrderofMag-1].gold[highestUsedOrderofMag]
+func _update_goldText():
+	var largestNumAt = 0;
+	var gold_text_formatted = "";
+	var keyNow = "";
 
 
+	# !!!!for loops format the text, fix later please 
+	# This and below can be made simpler somehow, and more efficient !!!!
+	# vvvvv
+	for key in gold:
+		if gold[key] >= 1:
+			keyNow = key;
+	
+	for num in range(gold.values().size()):
+		
+		if gold.values()[num] >= 1:
+			largestNumAt = num 
+			
+	
+	gold_text_formatted = str(gold.values()[largestNumAt],".",
+						gold.values()[largestNumAt-1]," ",keyNow)
+	
+	#^^^^^^^^^^^^^
+	# !!!! FIX UP TO HERE !!!! 
+	
+	print(largestNumAt)
+	print("formatted text:",gold_text_formatted,"\n",
+			#"prevkey: ",prevKey,"\n",
+			"keynow: ",keyNow,"\n",
+			#"emptykey: ",emptyKey,"\n"
+			)
+	print(gold.values())
+	#this gold should be the formatted version
+	gold_text.text = str(gold_text_formatted,"\n",gold)
+
+
+# This function should be called by gold making entities after they add gold 
+# to the above dictionary
 func goldOverCheck():
 	# Amount of millions in the value of Thousands
 	var thousandsCheck = gold["Thousands"]/1000000;
@@ -42,8 +83,8 @@ func goldOverCheck():
 	# Amount of thousands in the value of other between numbers
 	var overflow = 0;
 	
-	print("thous ",thousandsCheck)
-	print("inf ",infinitiesCheck)
+	#print("thous ",thousandsCheck)
+	#print("inf ",infinitiesCheck)
 	for key in gold:
 		
 		# if something before us overflowed, take the overflow and add to self
@@ -75,14 +116,19 @@ func goldOverCheck():
 			if key != "True Infinity" && gold[key] > 999:
 				overflow = gold[key]/1000
 				gold[key] -= overflow*1000
-				print("I'm in 999")
+				print("I'm in 999 and ", key, " has been overflowed")
 				
+	_update_goldText()
 
 
+# Below is a test of how each entity would call goldOverCheck()
+# add gold to gold dict, then overflow check. Each entity may have a timer,
+# or check for a global one.
 func _on_timer_timeout() -> void:
 	print("hello " , curTime)
-	goldOverCheck();
+	
 	print(gold)
 	gold["Thousands"] += 0;
 	gold["Millions"] += 90860;
+	goldOverCheck();
 	curTime+=1;
